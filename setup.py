fcef06e8142b336ee66632a5513bfc6b5d82ec7f
João Magalhães <joamag@gmail.com>
2021-01-26 09:23:25 +0000

    version: 0.2.1

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ import setuptools
 
 setuptools.setup(
     name = "s3_api",
-    version = "0.2.0",
+    version = "0.2.1",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Alibaba Cloud API Client",
